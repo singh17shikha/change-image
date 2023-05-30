d9fb95fb7441bff5bc1be822228ddbb1373f4e95
singh17shikha <130738166+singh17shikha@users.noreply.github.com>
2023-05-30 09:49:40 +0530

    Add files via upload

---
+++ src/list.js
@@ -0,0 +1,24 @@
+import from './list.css';
+
+
+import {Link} from 'react-router-dom'
+
+const List =()=>{
+
+    <nav>
+
+        <ul>
+<li>
+    <Link to = "/">Singup</Link>
+</li>
+<li>
+    <Link to = "/login">Login</Link>
+</li>
+<li>
+    <Link to = "/resister">Home</Link>
+</li>
+        </ul>
+
+    </nav>
+};
+export default List;
